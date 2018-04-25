3ac590b4834769041a25e40d8881fb1ddf9d98c6
zhaoy <2585068770@qq.com>
2018-04-25 22:39:39 +0800

    test1

--- app/src/main/java/com/test/git/MainActivity.java
+++ app/src/main/java/com/test/git/MainActivity.java
@@ -2,15 +2,13 @@ package com.test.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    private void test(){
-
     }
 }
